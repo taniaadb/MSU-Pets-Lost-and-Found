39294c4af411911d88355ca9a011ba28d9331450
duytantran <duy.tan.tran@itera.no>
2020-08-19 15:25:04 +0200

    feat: add pets Models/pets.cs class

---
+++ PetsLostAndFound/PetsLostAndFound/Models/Pets.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetsLostAndFound.Models
+{
+    public class Pets // use partial keyword
+    {
+        public int Id { get; set; }
+
+
+    }
+}
