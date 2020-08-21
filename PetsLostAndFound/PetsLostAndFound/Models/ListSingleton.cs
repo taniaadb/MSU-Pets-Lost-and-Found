3328f964097715d8ec6d34a24a300c21eb715659
duytantran <duy.tan.tran@itera.no>
2020-08-21 13:13:32 +0200

    feat: add singleton list

---
+++ PetsLostAndFound/PetsLostAndFound/Models/ListSingleton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetsLostAndFound.Models
+{
+    // Singleton to contain data for display
+    public class ListSingleton
+    {
+        private List<Animal> _allAnimals;
+
+        private static ListSingleton _instance = null;
+    }
+}
