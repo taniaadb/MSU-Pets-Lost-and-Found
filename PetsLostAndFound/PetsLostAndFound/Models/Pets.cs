28599a6acd5ae3e5c3a866a68bc303d72c1a4d8a
Tania Adelina Bulz <tania.bulz@xledger.com>
2020-08-21 11:20:02 +0200

    Added pet properties

--- PetsLostAndFound/PetsLostAndFound/Models/Pets.cs
+++ PetsLostAndFound/PetsLostAndFound/Models/Pets.cs
@@ -8,6 +8,13 @@ namespace PetsLostAndFound.Models
     public class Pets // use partial keyword
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public bool Microchipped { get; set; }
+        public string Microchip { get; set; }
+        public string RegisterId { get; set; }
+
 
 
     }
