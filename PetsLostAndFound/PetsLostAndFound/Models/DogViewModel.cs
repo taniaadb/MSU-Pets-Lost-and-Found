7f3e433187193d6fa83c460d40d60f20b18d2ded
duytantran <duytan95@hotmail.com>
2020-08-31 15:44:58 +0200

    feat: add dogviewmodel

---
+++ PetsLostAndFound/PetsLostAndFound/Models/DogViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetsLostAndFound.Models
+{
+    public class DogViewModel
+    {
+        public string PetName { get; set; }
+        public int Age { get; set; }
+        public BaseType Type { get; set; }
+        public string Breed { get; set; }
+        public string Description { get; set; }
+        public bool Microchipped { get; set; }
+        public string Microchip { get; set; }
+        public string RFID { get; set; }
+
+        // TO DO: Link the dog list to a database
+        public List<Dog> DogList { get; set; }
+    }
+}
