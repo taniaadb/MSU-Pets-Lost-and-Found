2fe2ade17265cfbc940066351d73e78c6dd5f52e
duytantran <duytan95@hotmail.com>
2020-09-02 13:31:07 +0200

    merge Models/files

---
+++ PetsLostAndFound/PetsLostAndFound/Models/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetsLostAndFound.Models
+{
+    public abstract class Animal // use partial keyword ?
+    {
+        // TO DO: Should add a [Required] field
+        public int Id { get; set; }
+        public string PetName { get; set; }
+        public string ImageUrl { get; set; }
+        public BaseType Type { get; set; }
+        public string Breed { get; set; }
+        public string Description { get; set; }
+        public bool Microchipped { get; set; }
+        public string Microchip { get; set; }
+        public string RFID { get; set; }
+
+    }
+
+    public enum BaseType
+    {
+        Dog, 
+        Cat,
+        Bird,
+        Other
+    }
+}
---
+++ PetsLostAndFound/PetsLostAndFound/Models/ListSingleton.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace PetsLostAndFound.Models
+{
+    // Singleton to contain data for display
+    public class ListSingleton
+    {
+        private List<Animal> _allAnimals;
+
+        private static ListSingleton _instance = null;
+
+        public static ListSingleton Instance 
+        {
+            get 
+            {
+                if (_instance == null)
+                {
+                    _instance = new ListSingleton();
+                }
+                return _instance;
+            }
+             }
+
+        private ListSingleton()
+        {
+            _allAnimals = new List<Animal>()
+            {
+                new Dog()
+                {
+                    Id = 1,
+                    PetName = "Dog Pet name",
+                    ImageUrl = "",
+                    Type = BaseType.Dog,
+                    Breed = "",
+                    Description = "",
+                    Microchipped = false,
+                    Microchip = "",
+                    RFID = ""
+                },
+
+                new Cat()
+                {
+                    Id = 2,
+                    PetName = "Cat Pet name",
+                    ImageUrl = "",
+                    Type = BaseType.Cat,
+                    Breed = "",
+                    Description = "",
+                    Microchipped = false,
+                    Microchip = "",
+                    RFID = ""
+                },
+
+                new Bird()
+                {
+                    Id = 3,
+                    PetName = "Bird Pet name",
+                    ImageUrl = "",
+                    Type = BaseType.Bird,
+                    Breed = "",
+                    Description = "",
+                    Microchipped = false,
+                    Microchip = "",
+                    RFID = ""
+                },
+
+                new Other()
+                {
+                    Id = 1,
+                    PetName = "Other Pet name",
+                    ImageUrl = "",
+                    Type = BaseType.Other,
+                    Breed = "",
+                    Description = "",
+                    Microchipped = false,
+                    Microchip = "",
+                    RFID = ""
+                }
+            };
+        }
+
+        public List<Animal> GetAllAnimals()
+        {
+            return _allAnimals;
+        }
+
+
+
+
+       
+    }
+}
